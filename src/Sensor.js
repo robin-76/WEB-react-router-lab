34004eb76328acc1d074aa1a3a2ee4faeddb5448
Robin Guyomar <robin.guyomar@etu.univ-lehavre.fr>
2021-04-29 22:12:30 +0200

    Modif Sensor.js

--- src/Sensor.js
+++ src/Sensor.js
@@ -2,23 +2,23 @@ import React from "react";
 import {useParams} from "react-router-dom";
 
 function Sensor(props){
-  let {id} = useParams();
-  let sensor = props.sensors.filter(s => {
-    return s.id === id;
-  })[0];
-  return (
-    <div className='sensors'>
-    <h3>{sensor.name}</h3>
-    <p>Valeur actuelle :</p>
-    <h4>{sensor.values[sensor.values.length-1]}</h4>
-    <p>Historique :</p>
-    <table>
-    {sensor.values.slice().reverse().map(value=>{
-      return <tr><td>{value}</td></tr>;
-    })}
-    </table>
-    </div>
-  );
+    let {id} = useParams();
+    let sensor = props.sensors.filter(s => {
+        return s.id === id;
+    })[0];
+    return (
+        <div class='sensors'>
+            <h3>{sensor.name}</h3>
+            <p>Valeur actuelle :</p>
+            <h4>{sensor.values[sensor.values.length-1]}</h4>
+            <p>Historique :</p>
+            <table>
+            {sensor.values.slice().reverse().map(value=>{
+                return <tr><td>{value}</td></tr>;
+            })}
+            </table>
+        </div>
+    );
 }
 
 export default Sensor;
