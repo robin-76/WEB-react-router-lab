91be33918515c45b5a29ab87b1f3aded722dd344
Maxence Bourgeaux <maxence.bourgeaux@etu.univ-lehavre.fr>
2021-05-03 11:19:15 +0200

    Quelques modifs légères + tests pour la classe App

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import React from "react";
+import {render, screen} from '@testing-library/react';
 import Menu from "./Menu";
+import App from "./App";
+import React from "react";
 import {
     BrowserRouter as Router
 } from "react-router-dom";
 
-test('renders learn react link', () => {
+test('Menu + correct Sensor', () => {
     let menu = React.createRef();
     render(<Router><Menu ref={menu}/></Router>);
     const sensors = [
@@ -23,5 +24,35 @@ test('renders learn react link', () => {
     ];
     menu.current.changeState(sensors);
     const linkElement = screen.getByText(/Temperature Salle A111/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('Menu + null Sensor', () => {
+    const menu = React.createRef();
+    render(<Router><Menu ref={menu}/></Router>)
+    const menuTest = [];
+    menu.current.changeState(menuTest);
+    const linkElement = screen.queryByText("Link");
+    expect(linkElement).toBeNull();
+});
+
+test('App + correct Sensor', () => {
+    let app = React.createRef();
+    render(<Router><App ref={app}/></Router>);
+    const sensors = [
+        {
+            "id": "123456",
+            "name": "Temperature Couloir",
+            "type": "DOUBLE",
+            "values": [
+                "18.45",
+                "45.23",
+                "-12.45",
+                "0.12",
+            ]
+        }
+    ];
+    app.current.changeState(sensors);
+    const linkElement = screen.getByText(/Temperature Couloir/i);
     expect(linkElement).toBeInTheDocument();
 });
