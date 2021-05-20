374f58a002068fa39971e4c60892ed07a900f932
Robin Guyomar <robin.guyomar@etu.univ-lehavre.fr>
2021-05-20 18:18:07 +0200

    Mise à jour CSS avec GridLayout

--- src/Menu.js
+++ src/Menu.js
@@ -8,15 +8,16 @@ function Menu(props) {
         setSensors(sensors);
     }
 
-        return (
-            <div className='boutons'>
-                <ul>
-                    {sensors.map((sensor)=>{
-                        return <li key = {sensor.name}><Link to={sensor.name.replace(/\s/g, "")}>{sensor.name}</Link></li>;
-                    })}
-                </ul>
-            </div>
-        );
+    return (
+      <div className="wrapper">
+        <div className="boutons"><ul>
+            {sensors.map((sensor)=>{
+                return <li key = {sensor.name}><Link to={sensor.name.replace(/\s/g, "")}>{sensor.name}</Link></li>;
+            })}
+        </ul></div>
+        </div>
+    );
+
 }
 
 export default Menu;
--- src/Sensor.js
+++ src/Sensor.js
@@ -8,19 +8,20 @@ function Sensor(props) {
     })[0];
 
     return (
-        <div className='sensors'>
-            <h3>{sensor.name}</h3>
-            <p>Valeur actuelle :</p>
-            <h4>{sensor.values[sensor.values.length-1]}</h4>
-            <p>Historique :</p>
-            <table>
-                <tbody>
-                {sensor.values.slice().reverse().map(value=>{
-                    return <tr><td>{value}</td></tr>;
-                })}
-                </tbody>
-            </table>
+      <div className="wrapper">
+        <div className="nomSensor"><h3>{sensor.name}</h3></div>
+        <div className="textValeurActuelle"><p>Valeur actuelle :</p></div>
+        <div className="valeurSensor"><h4>{sensor.values[sensor.values.length-1]}</h4></div>
+        <div className="historique"><p>Historique :</p></div>
+        <div className="tableau"><table>
+            <tbody>
+            {sensor.values.slice().reverse().map(value=>{
+                return <tr key={value}><td>{value}</td></tr>;
+            })}
+            </tbody>
+        </table>
         </div>
+      </div>
     );
 }
 
