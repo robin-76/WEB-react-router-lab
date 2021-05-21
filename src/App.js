9599d14c8a02b8b001a1530d4241284114097789
Robin Guyomar <robin.guyomar@etu.univ-lehavre.fr>
2021-05-21 23:10:59 +0200

    Modif champ Broker

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
             }
 
           }
-          connexion()
+          connexion();
         }
       }
       window.addEventListener('keydown', onKeyDown);
--- src/Broker.js
+++ src/Broker.js
@@ -4,7 +4,7 @@ const Broker = props => {
     return (
         <div className="wrapper">
             <div className="textURL"><h2>URL du Broker :</h2></div>
-            <div className="champURL"><input type="text" size="35" placeholder="ws://random.pigne.org:9001" onKeyDown={props.onKeyDown} /></div>
+            <div className="champURL"><input type="text" size="35" value="ws://random.pigne.org:9001" onKeyDown={props.onKeyDown} /></div>
         </div>
     );
 };
