e7630aef4b3f9202c651058141b7ca88c4014451
Maxence Bourgeaux <maxence.bourgeaux@etu.univ-lehavre.fr>
2021-05-21 20:00:25 +0200

    Corrections AppTest

--- src/App.test.js
+++ src/App.test.js
@@ -5,21 +5,7 @@ import React from "react";
 test('Test classe App + correct Sensor', () => {
     let app = React.createRef();
     render(<App />);
-    const sensors = [
-        {
-            "id": "123456",
-            "name": "Temperature Couloir",
-            "type": "DOUBLE",
-            "values": [
-                "18.45",
-                "45.23",
-                "-12.45",
-                "0.12",
-            ]
-        }
-    ];
 
-    app.current.changeSensors(sensors);
-    const linkElement = screen.getByText(/TP/i);
+    const linkElement = screen.getByText(/TP Lab React et React Router/i);
     expect(linkElement).toBeInTheDocument();
 });
